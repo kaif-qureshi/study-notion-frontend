4a931f934e7efe1a5d2129c869f0be6a00b3a920
kaif-qureshi <qureshikaif151@gmail.com>
2026-02-06 19:37:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import {Route, Routes,} from "react-router-dom";
+import Home from "./pages/Home"
+
+function App() {
+  
+  return (
+    <>
+
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter" >
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+      
+      </Routes>
+
+
+
+    </div>
+
+    
+      
+       
+    </>
+  )
+}
+
+export default App
